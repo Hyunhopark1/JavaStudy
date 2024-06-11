dbd3fbf22d84b4e3970d86a5177502eef5abda36
Hyunhopark1 <phh1569@nate.com>
2024-06-11 21:34:55 +0900

    add exam120817,120829,120831

---
+++ src/main/java/com/maven/springboot/study01/mathexam/Student.java
@@ -0,0 +1,24 @@
+package com.maven.springboot.study01.mathexam;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter //getter메소드 자동생성 어노테이션
+public class Student {
+    private String name;
+    private int score;
+
+
+    public Student(String name, int score) {
+        this.name=name;
+        this.score=score;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("이름 : %s, 점수 : %d", name, score);
+    }
+}
