17adba0ec509cc54ed5db586de0487c4e3bfc4f1
Hyunhopark1 <phh1569@nate.com>
2024-08-06 10:08:03 +0900

    delete icommon

--- mymustache/src/main/java/com/maven/springboot/mymustache/category/ICategoryService.java
+++ mymustache/src/main/java/com/maven/springboot/mymustache/category/ICategoryService.java
@@ -3,7 +3,12 @@ package com.maven.springboot.mymustache.category;
 import java.util.List;
 
 
-public interface ICategoryService<T> extends ICommonService<T> {
+public interface ICategoryService<T> {
+    T findById(Long id);
+    List<T> getAllList();
+    T insert(T dto) throws Exception;
+    Boolean delete(Long id) throws Exception;
+    T update(Long id, T dto) throws Exception;
     ICategory findByName(String name);
     List<ICategory> findAllByNameContains(SearchCategoryDto dto);
     int countAllByNameContains(SearchCategoryDto searchCategoryDto);
